d620ad016daeb497bc9df22ce4da1aafb5fd10f9
Will Sluder <will.sluder@gmail.com>
2016-11-17 15:34:44 +0000

    Implementing usage of PictureTakenActivity

--- app/src/main/java/com/snapsigns/SnapSigns.java
+++ app/src/main/java/com/snapsigns/SnapSigns.java
@@ -1,18 +1,52 @@
 package com.snapsigns;
 
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 
-public class SnapSigns extends android.app.Application {
+public class SnapSigns extends android.app.Application implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
 
+
+    public GoogleApiClient getmGoogleApiClient() {
+        return mGoogleApiClient;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
